7c8cd9207c89d426f23bf6febef52dfbc4589b7f
maanne0289 <71934782+maanne0289@users.noreply.github.com>
2020-09-27 01:25:59 +0800

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "H";
+    cout << "e";
+    cout << "l";
+    cout << "l";
+    cout << "o \n";
+    cout << "W";
+    cout << "o";
+    cout << "r";
+    cout << "l";
+    cout << "d";
+    
+    system("pause");
+    return EXIT_SUCCESS;
+}
---
+++ main3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello \n";
+    cout << "W";
+    cout << "o";
+    cout << "r";
+    cout << "l";
+    cout << "d";
+   
+   system("pause");
+    return EXIT_SUCCESS;
+}
